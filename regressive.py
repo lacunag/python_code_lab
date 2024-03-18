c3d3f12a7021ab0d1e6d4cf376d04367c5deaaa8
Luis Acuna <luisacuna@Mac-mini-de-Luis.local>
2024-03-18 16:45:49 -0300

    update regressive

--- regressive.py
+++ regressive.py
@@ -3,17 +3,18 @@ import time
 def countdown(t):
     while t > 0:
         print(t)
-        time.sleep(1)  # Espera 1 segundo
+        time.sleep(1)  # Wait for 1 second
         t -= 1
 
 while True:
-    tiempo_ingresado = int(input("Ingrese el tiempo de cuenta regresiva en segundos (o ingrese 0 para salir): "))
-    if tiempo_ingresado == 0:
-        break  # Salir del bucle si el usuario ingresa 0
-    countdown(tiempo_ingresado)
+    time_input = int(input("Ingrese el tiempo de cuenta regresiva en segundos (o ingrese 0 para salir): "))
+    if time_input == 0:
+        break  # Exit the loop if the user enters 0
+    countdown(time_input)
     
-    continuar = input("¿Desea continuar? (s/n): ")
-    if continuar.lower() != 's':
-        break  # Salir del bucle si el usuario no desea continuar
+    continue_option = input("¿Desea continuar? (s/n): ")
+    if continue_option.lower() != 'y':
+        break  # Exit the loop if the user does not want to continue
 
 print("¡Cuenta regresiva finalizada!")
+
