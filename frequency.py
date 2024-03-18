1bd475401acb1b0114e861db5de06ba90a6de35a
Luis Acuna <luisacuna@Mac-mini-de-Luis.local>
2024-03-18 16:40:35 -0300

    update frequency

--- frequency.py
+++ frequency.py
@@ -1,18 +1,18 @@
 from collections import Counter
 import re
 
-def analizar_frecuencia_palabras(archivo):
-    with open(archivo, 'r') as f:
-        texto = f.read().lower()
-        palabras = re.findall(r'\b\w+\b', texto)
-        contador = Counter(palabras)
-        palabras_comunes = contador.most_common()
-        return palabras_comunes
+def analyze_word_frequency(file):
+    with open(file, 'r') as f:
+        text = f.read().lower()
+        words = re.findall(r'\b\w+\b', text)
+        counter = Counter(words)
+        common_words = counter.most_common()
+        return common_words
 
 
-archivo = input("Ingrese el nombre del archivo de texto y su extension (txt): ")
-palabras_comunes = analizar_frecuencia_palabras(archivo)
+file = input("Ingrese el nombre del archivo de texto y su extension (txt): ")
+common_words = analyze_word_frequency(file)
     
 print("Palabras más comunes:")
-for palabra, frecuencia in palabras_comunes:
-    print(f"- {palabra}: {frecuencia}")
+for word, frequency in common_words:
+    print(f"- {word}: {frequency}")
